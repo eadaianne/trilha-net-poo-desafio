2a20c711fee683054f4cf2a3b3e917e0eab09857
eadaianne <eadaianne@gmail.com>
2024-12-13 10:39:16 -0300

    Implementa desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("(62) 98635-2141", "Nokia A12", "887799664455", 15);
+
+nokia.InstalarAplicativo("Youtube");
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+Iphone iphone = new Iphone("(61) 98465-2771", "Iphone 16", "5615512315", 23);
+
+iphone.InstalarAplicativo("Youtube");
+iphone.Ligar();
+iphone.ReceberLigacao();
